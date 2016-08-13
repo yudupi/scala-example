82a8307326fab2480cb206987ec7b4c729285f82
yudupi <yudupi@cisco.com>
2016-08-12 18:07:40 -0700

    A simple app to demonstrate Kafka Publisher and Subscriber in Scala

--- kafka-pubsub/.worksheet/src/com.example.scala.scala_example.test.scala
+++
@@ -1,5 +0,0 @@
-package com.example.scala.scala_example
-
-object test {;import org.scalaide.worksheet.runtime.library.WorksheetSupport._; def main(args: Array[String])=$execute{;$skip(98); 
-  println("Welcome to the Scala worksheet")}
-}
